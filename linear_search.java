8981328265c18a711c610fa9348f0b17dd6e364b
Vermaji <101314398+Vermaji8620@users.noreply.github.com>
2023-10-17 10:16:35 +0530

    Create linear_search.java

---
+++ linear_search.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found, return the index
+            }
+        }
+        return -1; // Target not found in the array
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int target = 10;
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
